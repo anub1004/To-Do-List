a3926058a2d734d7ed35cabdf388085762b3df41
anub1004 <anubhavsingh6260@gmail.com>
2025-08-20 15:30:16 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,116 @@
+let inputbox = document.querySelector("input");
+let list = document.querySelector('.listtask');
+let au = document.querySelector('audio');
+let dt=document.getElementById('dt');
+function add() {
+    if (inputbox.value === '' || dt.value ==='') {
+        tasknotgive();
+        alert("Please Enter Task");
+    }
+    else {
+        taskadd();
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        list.appendChild(li);
+        
+           let sdt=document.createElement('P');
+           sdt.innerHTML=dt.value;
+           sdt.classList.add('dt');
+           li.appendChild(sdt);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        span.classList.add("close");
+        li.appendChild(span);
+           
+    }
+    inputbox.value = "";
+    saves();
+
+}
+list.addEventListener("click", function (e) {
+    if (e.target.tagName === 'LI' ) {
+        e.target.classList.toggle("checked");
+        saves();
+
+        if (e.target.classList == 'checked') {
+            playTaskCompleted();
+        }
+        
+      
+    }
+    else if (e.target.tagName === 'SPAN') {
+        taskdelete();
+        e.target.parentElement.remove();
+        saves();
+    }
+}, false);
+
+function saves(){
+    localStorage.setItem("data",list.innerHTML);
+}
+function show(){
+    list.innerHTML=localStorage.getItem("data");
+}
+show();
+function playTaskCompleted() {
+    let msg = new SpeechSynthesisUtterance("Task Completed");
+
+    msg.lang = 'en-US';
+    msg.pitch = 1;
+    msg.rate = 1;
+    msg.volume = 1;
+    window.speechSynthesis.speak(msg);
+}
+function taskadd() {
+    let msg = new SpeechSynthesisUtterance("Task Added");
+    window.speechSynthesis.speak(msg);
+}
+function taskdelete() {
+    let msg = new SpeechSynthesisUtterance("Task deleted");
+    window.speechSynthesis.speak(msg);
+}
+function tasknotgive() {
+    let msg = new SpeechSynthesisUtterance("Please Enter a Task");
+    window.speechSynthesis.speak(msg);
+}
+
+
+setInterval(check, 1000);
+
+function check() {
+  let now = new Date();
+
+
+  let tasks = document.querySelectorAll(".listtask li");
+
+  tasks.forEach(li => {
+    let dtElem = li.querySelector(".dt");
+    if (!dtElem) return;
+
+    let inputDate = new Date(dtElem.innerText);
+
+    if (
+      now.getFullYear() === inputDate.getFullYear() &&
+      now.getMonth() === inputDate.getMonth() &&
+      now.getDate() === inputDate.getDate() &&
+      now.getHours() === inputDate.getHours() &&
+      now.getMinutes() === inputDate.getMinutes()
+    ) {
+      if (!li.dataset.alerted) {
+        li.dataset.alerted = "true";  
+        au.play();
+
+        setTimeout(() => {
+          au.pause();
+          au.currentTime = 0; 
+        }, 10000);
+      }
+    }
+    else if (
+      now > inputDate
+    ) {
+      li.remove();
+      saves();
+    }
+  });
+}
